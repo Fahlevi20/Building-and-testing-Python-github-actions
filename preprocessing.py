d5fc44ef026a49da453f5035748d1d929f1ed767
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-03 17:18:03 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,15 +2,15 @@
 # Import libraries
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-boston = load_boston()
-data = pd.DataFrame(boston.data, columns=boston.feature_names)
-data['PRICE'] = boston.target
+housing = fetch_california_housing()
+data = pd.DataFrame(housing.data, columns=boston.feature_names)
+data['PRICE'] = housing.target
 
 X = data.drop("PRICE", axis=1)
 y = data["PRICE"]
