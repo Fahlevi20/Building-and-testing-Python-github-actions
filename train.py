a8ccfb68089788308913d04ef87c09eac2c9b78c
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-03 17:35:36 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from processing import process_data
+from preprocessing import process_data
 import joblib
 process_data()
 
