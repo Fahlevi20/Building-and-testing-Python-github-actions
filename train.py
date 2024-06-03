78112c7f06f0544c6b7c8cf5f474ed84d31a0964
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-03 17:39:24 +0700

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from preprocessing import process_data
 import joblib
-process_data()
+X_train,X_test,y_train,y_test=process_data()
 
 model = LinearRegression()
 model.fit(process_data.X_train, process_data.y_train)
