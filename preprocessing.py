e1b31f1f85b126c4a204dd385bea05dd979e7a47
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-03 17:29:07 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -8,15 +8,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-housing = fetch_california_housing()
-data = pd.DataFrame(housing.data, columns=housing.feature_names)
-data['PRICE'] = housing.target
-
-X = data.drop("PRICE", axis=1)
-y = data["PRICE"]
-
-X_train,X_test,y_train,y_test = train_test_split(X,y, random_state=42)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test=scaler.fit_transform(X_test)
+def process_data():
+  housing = fetch_california_housing()
+  data = pd.DataFrame(housing.data, columns=housing.feature_names)
+  data['PRICE'] = housing.target
+  
+  X = data.drop("PRICE", axis=1)
+  y = data["PRICE"]
+  
+  X_train,X_test,y_train,y_test = train_test_split(X,y, random_state=42)
+  
+  scaler = StandardScaler()
+  X_train = scaler.fit_transform(X_train)
+  X_test=scaler.fit_transform(X_test)
