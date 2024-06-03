3081575668dd6613cee6cf3f49ad9aa1b3d621f9
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-03 17:11:40 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,8 @@
 # import dataset
-
+# Import libraries
+import numpy as np
 import pandas as pd
-from sklearn.dataset import load_boston
+from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
