39d3d9edbd3e6cbf7c24bc3501c2d28b45716d99
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-03 17:07:03 +0700

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,21 @@
+# import dataset
+
+import pandas as pd
+from sklearn.dataset import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+boston = load_boston()
+data = pd.DataFrame(boston.data, columns=boston.feature_names)
+data = ['PRICE'] = boston.target
+
+X = data.drop("PRICE", axis=1)
+y = data["PRICE")
+
+X_train,X_test,y_train,y_test = train_test_split(X,y, random_state=42)
+
+scaler = StandarScaler()
+X_train = scaler.fit_transform(X_train)
+X_test=scaler.fit_transform(X_test)
