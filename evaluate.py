b3c544a09e018bed7a4af590dc572ed61dc30bf1
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-04 22:33:10 +0700

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -11,5 +11,5 @@ r_score = r2_score(y_test,y_pred)
 rmse = np.sqrt(mean_squared_error(y_test,y_pred))
 print(f'\nR^2={r_score}\nMean Square Error={mse}\nRoot Mean Square Error={rmse}.')
 
-with open('metrics.txt','w') as outfile:
+with open('evaluation_result.txt','w') as outfile:
   outfile.write(f'\nR^2={r_score}\nMean Square Error={mse}\nRoot Mean Square Error={rmse}.')
