450a276b9bd05e293b7fff3f1e7d71adc7ffc26d
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-06-06 15:59:08 +0700

    Update cleaning_outliers.py

--- cleaning_outliers.py
+++ cleaning_outliers.py
@@ -62,3 +62,18 @@ model_new = LinearRegression()
 model_new.fit(X_train_new, y_train_new)
 
 joblib.dump(model_new,"model_new.pkl")
+
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+model_new = joblib.load("model_new.pkl")
+
+y_pred_new=model_new.predict(X_test_new)
+mse = mean_squared_error(y_test_new,y_pred_new)
+r_score = r2_score(y_test_new,y_pred_new)
+rmse = np.sqrt(mean_squared_error(y_test_new,y_pred_new))
+print(f'\nR^2={r_score}\nMean Square Error={mse}\nRoot Mean Square Error={rmse}.')
+
+with open('metrics.txt','w') as outfile:
+  outfile.write(f'\nR^2={r_score}\nMean Square Error={mse}\nRoot Mean Square Error={rmse}.')
